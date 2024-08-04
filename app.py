39e20b1300354fb00f4373a094e19268d365124f
KavyaOleti <ramanjulukavya@gmail.com>
2024-08-04 12:03:55 -0700

    JWT authorization is implemented.

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 from sqlalchemy import Column, Integer, String, DateTime
-
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required, decode_token
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kavya'
+app.config['JWT_SECRET_KEY'] = 'kavya'
+jwt = JWTManager(app)
+
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'bookstore.db')
 db = SQLAlchemy(app)
@@ -70,6 +76,36 @@ def register():
     db.session.commit()
     return jsonify(new_user.todict()), 201
     
+    
+@app.route('/user/login', methods=['POST'])
+def login():
+    data=request.get_json()
+    
+    #Scenario where no details are entered
+    if not data:
+        return jsonify("No details are entered"),400
+    
+    email=data.get("email")
+    pwd=data.get("pwd")
+    
+    #Scenario where one of the details is missing
+    if email is None or pwd is None:
+        return jsonify("email or password is missing")
+    
+    x=user.query.filter_by(email=email,pwd=pwd).first()
+    
+    if x:
+        access_token=create_access_token(identity={'email':email})
+        return jsonify(access_token=access_token),200
+    return jsonify("Invalid credentials")
 
+@app.route('/profile', methods=['GET'])
+@jwt_required()
+def profile():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify({"msg": f"Hello, {current_user['email']}!"}), 200
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
